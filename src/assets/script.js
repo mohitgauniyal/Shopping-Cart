4805d6c86e1234aa87c4ce4aa468808f5d7d0b50
Mohit Gauniyal <mohitgauniyal19a@gmail.com>
2023-12-07 16:07:22 +0530

    fix pay()

--- src/assets/script.js
+++ src/assets/script.js
@@ -54,12 +54,15 @@ function getProductByIdFromList(productId, productList) {
   return productList.find((product) => product.productId === productId);
 }
 
+// Adds a product in the cart
 function addProductToCart(productId){
   let product = getProductByIdFromList(productId, products)
   if (product){
-  product.quantity += 1;
-  if(!cart.includes(product))
-    cart.push(product);
+    product.quantity += 1;
+
+    // adds the product in cart the first time clicks on Add To Cart option.
+    if(!cart.includes(product))
+      cart.push(product);
   }
 }
 
@@ -68,8 +71,10 @@ function addProductToCart(productId){
   - increaseQuantity should then increase the product's quantity
 */
 
+// Increase the quantity of a product in the cart
 function increaseQuantity(productId){
-  let product = getProductByIdFromList(productId, products)  
+  let product = getProductByIdFromList(productId, products)
+  // increments the quantity by 1, if the product is found
   if (product){
     product.quantity+=1;
   } 
@@ -86,6 +91,7 @@ function decreaseQuantity(productId){
   if (product){
     product.quantity-=1;
 
+    //checks if the product quantity reduces to 0, if it does, removes the product from the cart
     if(product.quantity===0)
       removeProductFromCart(productId);
   } 
@@ -98,9 +104,13 @@ function decreaseQuantity(productId){
 */
 
 function removeProductFromCart(productId){
+  // Find the index of the product in the cart array using its productId
   const index = cart.findIndex((product) => product.productId === productId);
+  // Check if the product exists in the cart (index !== -1 shows the product is found)
   if (index !== -1) {
+  // Set the quantity of the product to 0 if removed
   cart[index].quantity = 0;
+  // Remove the product from the cart. removes 1 product of the given index.
   cart.splice(index, 1);
 }
 }
@@ -112,7 +122,10 @@ function removeProductFromCart(productId){
 */
 
 function cartTotal(){
+  // initial cost is 0
   let cost = 0;
+
+  // pick each product in the cart, multiply its cost with the quantity, add them all up to get the final total cost.
   cart.forEach(function(item){
     cost += (item.price*item.quantity);
   });
@@ -131,9 +144,29 @@ function emptyCart(){
   Hint: cartTotal function gives us cost of all the products in the cart  
 */
 
-function pay(amount){
-  return amount-cartTotal();
+// Variable to track the total amount paid
+let totalPaid = 0;
+
+function pay(amount) {
+
+    // Add the current payment amount to the totalPaid variable
+    totalPaid += amount;
+
+    // Calculate the difference between the totalPaid and the cartTotal
+    let remaining = totalPaid - cartTotal()
+
+    // Check if the remaining amount is greater than or equal to zero
+    if (remaining >= 0) {
+        // If so, reset the `totalPaid` to zero to prepare it for the next
+        // payment, as the current payment is enough to cover the `cartTotal`.
+        totalPaid = 0;
+        emptyCart()
+    }
+
+    // Return the remaining (negative if payment is less than the cartTotal)
+    return remaining;
 }
+
 /* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
 
 
